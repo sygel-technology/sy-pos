ea20fb1f102a0f9e9c0564c7c2bd909c97c29db9
sygel <alberto.martinez@sygel.es>
2026-02-25 16:24:53 +0100

    [IMP] pos_items_counter: pre-commit auto fixes

--- pos_items_counter/__manifest__.py
+++ pos_items_counter/__manifest__.py
@@ -6,17 +6,17 @@
     "summary": "Show number of items in POS.",
     "version": "15.0.1.0.0",
     "category": "Custom",
-    "website": "https://www.sygel.es",
+    "website": "https://github.com/sygel-technology/sy-pos",
     "author": "Sygel",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
-        'point_of_sale',
+        "point_of_sale",
     ],
     "assets": {
-        'point_of_sale.assets': [
-            'pos_items_counter/static/src/js/ItemsCounter.js',
+        "point_of_sale.assets": [
+            "pos_items_counter/static/src/js/ItemsCounter.js",
         ],
         "web.assets_qweb": [
             "pos_items_counter/static/src/xml/ItemsCounter.xml",
--- pos_items_counter/models/uom_uom.py
+++ pos_items_counter/models/uom_uom.py
@@ -1,21 +1,17 @@
 # Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class UomUom(models.Model):
     _inherit = "uom.uom"
 
-    is_units = fields.Boolean(
-        compute="_compute_is_units",
-        readonly=True,
-        store=True
-    )
+    is_units = fields.Boolean(compute="_compute_is_units", readonly=True, store=True)
 
     def _compute_is_units(self):
-        units_uom_id = self.env.ref('uom.product_uom_unit')
-        for sel in self:            
+        units_uom_id = self.env.ref("uom.product_uom_unit")
+        for sel in self:
             is_units = False
             if units_uom_id and sel.id == units_uom_id.id:
                 is_units = True
